881f97e5efce187cf364bc561af0ae49d69ff126
Tolriq <tolriq@tolriq.com>
2018-03-02 19:11:52 +0100

    1.0.7: Add support for Youtube dl as secondary url resolver

--- script.yatse.kodi/lib/share.py
+++ script.yatse.kodi/lib/share.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 import utils
+import urlresolver
+import urllib
+import xbmc
+import xbmcgui
 
 from utils import logger
+
+have_youtube_dl = "false"
+try:
+    from youtube_dl import YoutubeDL
+    have_youtube_dl = "true"
+except ImportError:
+    logger.info("Youtube dl not present")
 
 def run(argument):
     if argument['type'] == 'magnet':
@@ -14,18 +25,26 @@ def run(argument):
         elif openWith == 'Quasar':
             utils.callPlugin('plugin://plugin.video.quasar/playuri?uri=' + argument['data'] + ')')
     elif argument['type'] == 'unresolvedurl':
-        import urlresolver
-        import urllib
-        import xbmc
-        import xbmcgui
         url = urllib.unquote(argument['data'])
         logger.info('Trying to resolve with urlresolver: %s' % url)
         stream_url = urlresolver.HostedMediaFile(url = url).resolve()
         if not stream_url:
-            dialog = xbmcgui.Dialog()
-            dialog.notification(utils.ADDON_NAME, utils.translation(32006), xbmcgui.NOTIFICATION_INFO, 5000)
-            logger.error("Url not resolved: %s" % url)
+            if have_youtube_dl == "false":
+                logger.error("Url not resolved and no YoutubeDL: %s" % url)
+            else:
+                logger.info('Trying to resolve with YoutubeDL: %s' % url)
+                YoutubeDL_resolver = YoutubeDL({'format': 'best'})
+                YoutubeDL_resolver.add_default_info_extractors()
+                result = YoutubeDL_resolver.extract_info(url, download = False)
+                if "url" in result:
+                    stream_url = result["url"]
+                    logger.info("Url resolved by YoutubeDL: %s" % result)
+                else:
+                    logger.error("Url not resolved: %s" % url)
         else:
+            logger.info("Url resolved by urlResolver: %s" % stream_url)
+
+        if stream_url:
             if (not 'queue' in argument) or (argument['queue'] == "false") or (not xbmc.Player().isPlaying()):
                 logger.info('Playing resolved url: %s' % stream_url)
                 xbmc.Player().play(stream_url)
@@ -36,3 +55,6 @@ def run(argument):
                 else:
                     logger.info('Queuing to video resolved url: %s' % stream_url)
                     xbmc.PlayList(xbmc.PLAYLIST_VIDEO).add(stream_url)
+        else:
+            dialog = xbmcgui.Dialog()
+            dialog.notification(utils.ADDON_NAME, utils.translation(32006), xbmcgui.NOTIFICATION_INFO, 5000)
