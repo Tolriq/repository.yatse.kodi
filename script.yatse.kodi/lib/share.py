b59569b8c40bd32277af0b3389e308df44fcf54a
Tolriq <tolriq@tolriq.com>
2018-03-16 21:58:42 +0100

    Add support for meta data Add support for Playlist Better unicode support Add busy indicator during resolution when nothing is playing

--- script.yatse.kodi/lib/share.py
+++ script.yatse.kodi/lib/share.py
@@ -3,26 +3,27 @@ import urllib
 
 import urlresolver
 import utils
+import xbmcgui
 from utils import logger
 
-have_youtube_dl = "false"
+have_youtube_dl = False
 try:
     # noinspection PyUnresolvedReferences
     import youtube_dl
 
-    have_youtube_dl = "true"
+    have_youtube_dl = True
 except ImportError:
-    logger.info("YoutubeDL not present")
+    logger.info('YoutubeDL not present')
 
 
 def run(argument):
     if argument['type'] == 'magnet':
         handle_magnet(argument['data'])
     elif argument['type'] == 'unresolvedurl':
-        if ('queue' not in argument) or (argument['queue'] == "false"):
-            action = "play"
+        if ('queue' not in argument) or (argument['queue'] == 'false'):
+            action = 'play'
         else:
-            action = "queue"
+            action = 'queue'
         handle_unresolved_url(argument['data'], action)
 
 
@@ -41,28 +42,53 @@ def handle_magnet(data):
 
 def handle_unresolved_url(data, action):
     url = urllib.unquote(data)
-    if have_youtube_dl == "true":
-        logger.info('Trying to resolve with YoutubeDL: %s' % url)
+    if not utils.kodi_is_playing():
+        dialog = xbmcgui.DialogBusy()
+        dialog.create()
+        dialog.update(-1)
+    else:
+        dialog = None
+    if have_youtube_dl:
+        logger.info(u'Trying to resolve with YoutubeDL: %s' % url)
         youtube_dl_resolver = youtube_dl.YoutubeDL({'format': 'best', 'no_color': 'true'})
         youtube_dl_resolver.add_default_info_extractors()
         try:
             result = youtube_dl_resolver.extract_info(url, download=False)
         except Exception as e:
-            logger.error("Error with YoutubeDL: %s" % e)
+            logger.error(u'Error with YoutubeDL: %s' % e)
             result = {}
-        if "url" in result:
-            logger.info("Url resolved by YoutubeDL: %s" % result)
-            utils.play_url(result['url'], action)
+        if 'entries' in result:
+            logger.info(u'Playlist resolved by YoutubeDL: %s items' % len(result['entries']))
+            item_list = []
+            for entry in result['entries']:
+                if 'url' in entry:
+                    item_list.append(entry)
+                    logger.info(u'Media found: %s' % entry['url'])
+            if len(item_list) > 0:
+                utils.play_items(item_list, action)
+                if dialog is not None:
+                    dialog.close()
+                return
+            else:
+                logger.info(u'No playable urls in the playlist')
+        if 'url' in result:
+            logger.info(u'Url resolved by YoutubeDL: %s' % result['url'])
+            utils.play_url(result['url'], action, result)
+            if dialog is not None:
+                dialog.close()
             return
-        else:
-            logger.error("Url not resolved by YoutubeDL: %s" % url)
+        logger.error(u'Url not resolved by YoutubeDL: %s' % url)
 
-    logger.info('Trying to resolve with urlResolver: %s' % url)
+    logger.info(u'Trying to resolve with urlResolver: %s' % url)
     stream_url = urlresolver.HostedMediaFile(url=url).resolve()
     if stream_url:
-        logger.info("Url resolved by urlResolver: %s" % stream_url)
+        logger.info(u'Url resolved by urlResolver: %s' % stream_url)
         utils.play_url(stream_url, action)
+        if dialog is not None:
+            dialog.close()
         return
 
-    logger.info("Trying to play as basic url: %s" % url)
+    logger.info(u'Trying to play as basic url: %s' % url)
     utils.play_url(url, action)
+    if dialog is not None:
+        dialog.close()
--- script.yatse.kodi/lib/utils.py
+++ script.yatse.kodi/lib/utils.py
@@ -24,7 +24,7 @@ class XBMCHandler(logging.StreamHandler):
         xbmc_level = self.xbmc_levels.get(record.levelname)
         if isinstance(record.msg, unicode):
             record.msg = record.msg.encode('utf-8')
-        if get_setting("logEnabled") == "true":
+        if get_setting('logEnabled') == 'true':
             xbmc.log(self.format(record), xbmc_level)
 
 
@@ -40,7 +40,7 @@ def get_setting(key):
 
 
 def call_plugin(plugin):
-    logger.info('Calling plugin: %s' % plugin)
+    logger.info(u'Calling plugin: %s' % plugin)
     xbmc.executebuiltin('XBMC.RunPlugin(%s)' % plugin)
 
 
@@ -56,18 +56,91 @@ def translation(id_value):
     return ADDON.getLocalizedString(id_value)
 
 
-def play_url(url, action):
+def kodi_is_playing():
+    return xbmc.Player().isPlaying()
+
+
+def play_url(url, action, meta_data=None):
+    if meta_data is not None:
+        list_item = get_kodi_list_item(meta_data)
+    else:
+        list_item = None
     if url:
-        if (action == "play") or (not xbmc.Player().isPlaying()):
-            logger.info('Playing url: %s' % url)
-            xbmc.Player().play(url)
+        if (action == 'play') or (not xbmc.Player().isPlaying()):
+            logger.info(u'Playing url: %s' % url)
+            # Clear both playlist but only fill video as mixed playlist will work and audio will correctly play
+            xbmc.PlayList(xbmc.PLAYLIST_MUSIC).clear()
+            playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+            playlist.clear()
+            if list_item is not None:
+                playlist.add(url, list_item)
+            else:
+                playlist.add(url)
+            xbmc.Player().play(playlist)
         else:
             if xbmc.Player().isPlayingAudio():
-                logger.info('Queuing to music url: %s' % url)
-                xbmc.PlayList(xbmc.PLAYLIST_MUSIC).add(url)
+                logger.info(u'Queuing url to music: %s' % url)
+                playlist = xbmc.PlayList(xbmc.PLAYLIST_MUSIC)
+                if list_item is not None:
+                    playlist.add(url, list_item)
+                else:
+                    playlist.add(url)
             else:
-                logger.info('Queuing to video url: %s' % url)
-                xbmc.PlayList(xbmc.PLAYLIST_VIDEO).add(url)
+                logger.info(u'Queuing url to video: %s' % url)
+                playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+                if list_item is not None:
+                    playlist.add(url, list_item)
+                else:
+                    playlist.add(url)
     else:
         dialog = xbmcgui.Dialog()
         dialog.notification(ADDON_NAME, translation(32006), xbmcgui.NOTIFICATION_INFO, 5000)
+
+
+def play_items(items, action):
+    if (action == 'play') or (not xbmc.Player().isPlaying()):
+        logger.info(u'Playing %s items' % len(items))
+        # Clear both playlist but only fill video as mixed playlist will work and audio will correctly play
+        xbmc.PlayList(xbmc.PLAYLIST_MUSIC).clear()
+        playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+        for item in items:
+            list_item = get_kodi_list_item(item)
+            playlist.add(list_item.getPath(), list_item)
+        xbmc.Player().play(playlist)
+    else:
+        if xbmc.Player().isPlayingAudio():
+            logger.info(u'Queuing %s items to music' % len(items))
+            playlist = xbmc.PlayList(xbmc.PLAYLIST_MUSIC)
+            for item in items:
+                list_item = get_kodi_list_item(item)
+                playlist.add(list_item.getPath(), list_item)
+        else:
+            logger.info(u'Queuing %s items to video' % len(items))
+            playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+            for item in items:
+                list_item = get_kodi_list_item(item)
+                playlist.add(list_item.getPath(), list_item)
+
+
+def get_kodi_list_item(meta_data):
+    list_item = xbmcgui.ListItem()
+    item_info = {}
+    if 'title' in meta_data:
+        list_item.setLabel(meta_data['title'])
+        item_info['title'] = meta_data['title']
+    if 'thumbnail' in meta_data:
+        list_item.setThumbnailImage(meta_data['thumbnail'])
+    if 'duration' in meta_data:
+        item_info['duration'] = meta_data['duration']
+    if 'url' in meta_data:
+        list_item.setPath(meta_data['url'])
+    if 'categories' in meta_data:
+        item_info['genre'] = meta_data['categories']
+    if 'average_rating' in meta_data:
+        item_info['rating'] = meta_data['average_rating']
+
+    if len(item_info) > 0:
+        list_item.setInfo('music', item_info)
+        list_item.setInfo('video', item_info)
+
+    return list_item
