35b447d50cb7c9e555d5fa16738e3be7eb8b8f65
Tolriq <tolriq@tolriq.com>
2021-01-11 12:13:38 +0100

    Update update script

--- update.ps1
+++ update.ps1
@@ -6,10 +6,24 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-Remove-Item temp\* -Recurse -Force
+Remove-Item .temp\* -Recurse -Force
 	
-Invoke-WebRequest -Uri "https://github.com/ytdl-org/youtube-dl/archive/master.zip" -OutFile "temp\ydl.zip"
+Invoke-WebRequest -Uri "https://github.com/ytdl-org/youtube-dl/archive/master.zip" -OutFile ".temp\ydl.zip"
 
-unzip "$PSScriptRoot\temp\ydl.zip" "$PSScriptRoot\temp"
+unzip "$PSScriptRoot\.temp\ydl.zip" "$PSScriptRoot\.temp"
 
-Copy-Item -Force -recurse "$PSScriptRoot\temp\youtube-dl-master\youtube_dl" -Destination "$PSScriptRoot\script.yatse.kodi\lib"
+Copy-Item -Force -recurse "$PSScriptRoot\.temp\youtube-dl-master\youtube_dl" -Destination "$PSScriptRoot\script.yatse.kodi\lib"
+
+$file = "version.txt"
+$fileVersion = (Get-Content $file | Select -First 1).Split(".")
+$fileVersion[2] = [int]$fileVersion[2] + 1
+$fileVersion -join "." | Set-Content $file
+$fileVersion = (Get-Content $file | Select -First 1)
+
+((Get-Content -path "script.yatse.kodi\addon.xml.template" -Raw) -replace '{Version}',$fileVersion) | Set-Content -Path "script.yatse.kodi\addon.xml"
+
+py repo_generator.py
+py repo_generator_matrix.py
+
+git add -A
+git commit -m "Sync youtubeDL"
